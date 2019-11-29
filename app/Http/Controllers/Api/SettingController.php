3ffa1e2a196603f11c071a4e57e3214d562037f0
saad salem <sa3dsalem01@gmail.com>
2019-11-29 20:50:52 +0200

    social

--- app/Http/Controllers/Api/SettingController.php
+++ app/Http/Controllers/Api/SettingController.php
@@ -3,28 +3,21 @@
 namespace App\Http\Controllers\Api;
 
 use App\Http\Controllers\Controller;
-use App\SavedPlaces;
 use App\Setting;
 use App\Social;
-use App\User;
 use Illuminate\Http\Request;
-use Illuminate\Support\Facades\Validator;
 
 class SettingController extends Controller
 {
     public function setting(Request $request)
     {
-
-
-
-
-        $setting=Setting::first();
-        $row['about']=$setting->about['ar'];
-        $row['mobile']=$setting->mobile;
-        $row['email']=$setting->email;
-        $row['facebook']=Social::where('name','facebook')->value('link');
-        $row['twitter']=Social::where('name','twitter')->value('link');
-        $row['insta']=Social::where('name','insta')->value('link');
+        $setting = Setting::first();
+        $row['about'] = $setting->about['ar'];
+        $row['mobile'] = $setting->mobile;
+        $row['email'] = $setting->email;
+        $row['facebook'] = Social::where('name', 'facebook')->value('link');
+        $row['twitter'] = Social::where('name', 'twitter')->value('link');
+        $row['insta'] = Social::where('name', 'insta')->value('link');
         return response()->json(['status' => 200, 'data' => $row]);
     }
 }
